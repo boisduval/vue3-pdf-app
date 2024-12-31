cbd5ac20ad762c1d444e862c853647405d347b94
boisduval <sniksil@126.com>
2025-01-01 04:43:02 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <PdfApp style="height: 90vh" pdf="/sample.pdf"></PdfApp>
+</template>
+
+<script>
+import PdfApp from "vue3-pdf-app";
+import "vue3-pdf-app/dist/icons/main.css";
+export default {
+  name: "App",
+  components: {
+    PdfApp,
+  },
+};
+</script>
